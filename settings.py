671a2ecdb05fe82ccb830a0872e8aa0902d9d3eb
Matt Adams <matt.adams@bitplatter.com>
2018-04-05 15:10:31 -0400

    Get token and feed url from environment variables

--- settings.py
+++ settings.py
@@ -1,2 +1,13 @@
-FLUID_TOKEN = ''
-FLUID_RSS_URL = '' 
+import os
+
+# Get your FluidDATA Token at https://accounts.bitplatter.com/home/security
+FLUID_TOKEN = None
+
+# Place a valid RSS feed url here
+FLUID_RSS_URL = None
+
+if FLUID_TOKEN is None:
+    FLUID_TOKEN = os.environ.get('FLUID_TOKEN', None)
+
+if FLUID_RSS_URL is None:
+    FLUID_RSS_URL = os.environ.get('FLUID_RSS_URL', None)
